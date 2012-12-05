61741013275e22a98a9db21f195c2a0278db2c18
adn <adn1107@gmail.com>
2012-12-05 10:37:16 -0800

    part 2

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "http://rubygems.org"
+gem "carrierwave-mongoid", :branch => "mongoid-3.0", :require => 'carrierwave/mongoid'
+gem "sinatra" 
+gem "haml"
+gem "json"
+gem "bson_ext"
--- helloworld.rb
+++ helloworld.rb
@@ -1,4 +1,64 @@
-require 'sinatra'
-get '/' do
-  "Hello World"
+require 'sinatra'   # required for sinatra classic detection in cloud foundry...
+require 'rubygems'
+require 'bundler'
+Bundler.require
+
+
+# Configure Mongoid
+Mongoid.configure do |config|
+  name = "testdb"
+  host = "localhost"
+  config.master = Mongo::Connection.new.db(name)
 end
+
+# Model:  Visit contains 1 or many Iprec
+class Visit
+ include Mongoid::Document
+ field :date, :type => String
+ field :count, :type => Integer
+ embeds_many :iprecs
+end
+
+#Model:  Iprec  embeded in Visit
+class Iprec
+ include Mongoid::Document
+ field :ip, :type => String
+ field :count, :type => Integer
+ embedded_in :visit
+end
+
+#Read (GET)
+ get '/' do
+    puts "page hit!"
+    
+    @visits= Visit.where(:date => Time.new.strftime("%m-%d-%Y")) 
+    
+    if (@visits.empty?)
+      puts 'add new record'
+      rec = Visit.new
+      rec.date = Time.new.strftime("%m-%d-%Y")
+      rec.count = 1
+      rec.save
+      rec.iprecs.create(ip: "#{request.ip}")
+    else
+    #if Visit.where(:date => Time.new.strftime("%m-%d-%Y")).exists?  
+      puts 'find existing record'
+      @visits[0].iprecs.create(ip: "#{request.ip}")
+      puts @visits[0].iprecs.count
+    end
+    
+    erb :index
+    
+=begin  :  debug showing records in Mongo db
+    puts "getting all days"
+    puts Visit.count
+    Visit.all.each do |member|
+      puts "#{member.date}" + "  " + "#{member.iprecs.count}" 
+    end 
+=end    
+    
+    
+ end
+
+
+
